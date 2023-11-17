a73d9d17c44fa3bab708fb4b984ba9cb757f4865
mokasp <kasperbouldin@gmail.com>
2023-11-17 13:39:24 -0600

    11-17-23 | 1:39pm | insert user input in func def

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,31 +4,26 @@ import requests
 import sys
 
 
-def main():
+def main(user_id):
     """ gets the todo list and name of specific employee """
-    arg = int(sys.argv[1])
     todo = []
     completed = []
     todo_r = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     user_r = requests.get('https://jsonplaceholder.typicode.com/users').json()
     for user in user_r:
-        if user['id'] == arg:
+        if user['id'] == user_id:
             name = user['name']
     for item in todo_r:
-        if item['userId'] == arg:
+        if item['userId'] == user_id:
             todo.append(item)
             if item['completed'] is True:
                 completed.append(item['title'])
     done = len(completed)
     all_ = len(todo)
-    with open('student_output', 'w') as f:
-        f.write(f'Employee {name} is done with tasks({done}/{all_})')
-        for item in completed:
-            f.write(f'\t {item}')
     print(f'Employee {name} is done with tasks({done}/{all_})')
     for item in completed:
         print(f'\t {item}')
 
 
 if __name__ == '__main__':
-    main()
+    main(int(sys.argv[1]))
