8ebd581ae9ed3b08ace67ecd315865ba13662b15
mokasp <kasperbouldin@gmail.com>
2023-11-17 13:07:36 -0600

    11-17-23 | 1:07pm | pycode style fixed

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
+""" script that extracts data from an api """
 import requests
 import sys
 arg = int(sys.argv[1])
 todo = []
 completed = []
-todo_response = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-user_response = requests.get('https://jsonplaceholder.typicode.com/users').json()
-for user in user_response:
+t_response = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+u_response = requests.get('https://jsonplaceholder.typicode.com/users').json()
+for user in u_response:
     if user['id'] == arg:
-        employee_name = user['name']
-for item in todo_response:
+        name = user['name']
+for item in t_response:
     if item['userId'] == arg:
         todo.append(item)
-        if item['completed'] == True:
+        if item['completed'] is True:
             completed.append(item['title'])
-print(f'Employee {employee_name} is done with tasks({len(completed)}/{len(todo)})')
+print(f'Employee {name} is done with tasks({len(completed)}/{len(todo)})')
 for item in completed:
     print(f'\t {item}')
